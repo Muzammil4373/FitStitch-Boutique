51af0944a93c3242899c7433f1902f1fcce420a2
Muzammil Shaikh <137262550+Muzammil4373@users.noreply.github.com>
2026-08-12 12:45:53 +0530

    type changes

--- src/pages/BusinessPartner.jsx
+++ src/pages/BusinessPartner.jsx
@@ -11,7 +11,7 @@ const initialForm = {
   pincode: '',
   phone: '',
   email: '',
-  businessType: '',
+  businessTypes: '',
   message: '',
 };
 
@@ -87,7 +87,7 @@ export default function BusinessPartner() {
         </div>
         <div>
           <label className={labelClass}>Business Type</label>
-          <select required name="businessType" value={form.businessType} onChange={handleChange} className={`${fieldClass} truncate`}/>
+          <select required name="businessType" value={form.businessTypes} onChange={handleChange} className={`${fieldClass} truncate`}/>
         </div>
         <div className="sm:col-span-2">
           <label className={labelClass}>Message</label>
